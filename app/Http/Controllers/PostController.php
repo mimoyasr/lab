2b9a98ef9a40aeae4075bcf7bbdf2884ad05254e
mimoyasr <amryasserhamza@gmail.com>
2018-04-19 06:50:25 +0200

    add delete post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -97,6 +97,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect(route('posts.index'));
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -24,12 +24,46 @@
                     <td> {{ $post->title }} </td>
                     <td> {{ $post->user->name }} </td>
                     <td> {{ $post->readable_date }} </td>
-                    <td id="actions" > 
-                        <a href='posts/{{ $post->id }}' class="btn btn-success">View</a>
-                        <a href='posts/{{ $post->id }}/edit' class="btn btn-primary">edit</a>
-                        <button class="btn btn-danger" targ='{{ $post->id }}'> delete</button>
+                    <td id="actions" >
+                        <div class="row">
+                            <div class="col-4">
+                                    <a href='posts/{{ $post->id }}' class="btn btn-success">View</a>
+
+                            </div>
+                            <div class="col-4">
+                                    <a href='posts/{{ $post->id }}/edit' class="btn btn-primary">edit</a>
+                            </div>
+                            <div class="col-4">
+                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete_{{ $post->id }}">
+                                Delete
+                            </button>
+                            </div>
                     </td>
             </tr>
+
+            <div class="modal fade" id="delete_{{ $post->id }}" tabindex="-1" role="dialog" 
+                    aria-labelledby="delete_post_{{ $post->id }}" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="delete_post_{{ $post->id }}">Delete Post</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                Do you want to Delete this post ?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <form action="/posts/{{$post->id}}" method="POST">
+                                    {{ method_field('DELETE') }}
+                                    {{ csrf_field() }}
+                                    <input type="submit" class="btn btn-danger" value="Delete">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
     
             @endforeach
             </tbody>
